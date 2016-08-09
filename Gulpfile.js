7710174f3926f7cb182c197ece17079c40530a80
Jared Rounsavall <jaredrounsavall@macbook-pro-2.dhcp.asu.edu>
2016-08-09 11:30:58 -0700

    Updating the uglp build to find branch 3

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var git = require('gulp-git');
 var gitrev = require('git-rev');
-var shel = require('gulp-shell');
+var shell = require('gulp-shell');
 
 gulp.task('default', function(){
     gitrev.branch(function (str) {
