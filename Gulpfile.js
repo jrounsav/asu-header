4052668b146c9eaa1ddbb3662ae7bb49a8bdbbbf
Jared Rounsavall <jaredrounsavall@macbook-pro-2.dhcp.asu.edu>
2016-08-09 11:14:50 -0700

    Updating the uglp build to find branch

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,9 +2,14 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var git = require('gulp-git');
 var gitrev = require('git-rev');
+var run = require('gulp-run');
 
 gulp.task('default', function(){
     gitrev.branch(function (str) {
-        gutil.log('branch', str)
-    })
+        gutil.log('not branch', str)
+    });
+    return run('echo $TRAVIS_BRANCH').exec()
+        .pipe(function(branch) {
+            gutil.log('this is branch ' + branch);
+        });
 });
