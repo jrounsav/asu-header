4433452066fd85a1b195b4810b4a1b237c0367fa
Jared Rounsavall <jaredrounsavall@MacBook-Pro-2.local>
2016-08-10 16:30:16 -0700

    Fixing the syntax

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 if [[ $TRAVIS_BRANCH == 'dev' ]]
+then
     gulp dev
 elif [[ $TRAVIS_BRANCH == 'qa' ]]
+then
     gulp qa
 elif [[ $TRAVIS_BRANCH == 'master' ]]
+then
     gulp master
 fi
